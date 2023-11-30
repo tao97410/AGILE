87af3acbf48495b906f38463f785ec5f7a0f000d
Entwaan <cremonaantoine@gmail.com>
2023-11-30 20:19:38 +0100

    dependance web inutile enlevée

--- agile/src/main/java/module-info.java
+++ agile/src/main/java/module-info.java
@@ -1,12 +1,11 @@
 module h4131 {
     requires transitive javafx.graphics;
-    requires transitive javafx.web;
     requires javafx.controls;
     requires javafx.fxml;
     requires java.xml;
 
     opens h4131.app to javafx.fxml;
-    opens h4131.view to javafx.fxml, javafx.web;
+    opens h4131.view to javafx.fxml;
     opens h4131.xml to java.xml, javafx.fxml, javafx.controls;
     
     exports h4131.app;
