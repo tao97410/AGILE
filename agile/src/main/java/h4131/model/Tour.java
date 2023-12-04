080ecb63dc03e5efee30900f12c1fadac172ebe0
tao97410 <tao.mouysset@insa-lyon.fr>
2023-12-04 19:00:42 +0100

    début fonction cost TSP pour alexis

--- agile/src/main/java/h4131/model/Tour.java
+++ agile/src/main/java/h4131/model/Tour.java
@@ -1,8 +1,10 @@
 package h4131.model;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
+import h4131.calculus.Arc;
 import h4131.calculus.Graph;
 
 public class Tour {
@@ -19,12 +21,9 @@ public class Tour {
     
     public int TSPDynamicProgramming(Graph initialGraph,LinkedList<DeliveryPoint> d, Map map){
          initialGraph=map.getGraphFromPoints(d);
+         Collection<DeliveryPoint> nodes= initialGraph.nodes;
+         Collection<Arc> arcs=initialGraph.arcs;
          
-
-
-
-
-
 
 
         return 0;
@@ -32,10 +31,23 @@ public class Tour {
     }
 
 
-    //Creates the cost function for the TSP
-    private int[][] createCost(){
+    // //Creates the cost function for the TSP
+    //  private int[][] createCost(Collection<DeliveryPoint> nodes, Collection<Arc> arcs){
+    //     Map<pair<DeliveryPoint,DeliveryPoint>,double> cost= new Map();
+    //     for(int i=0 ; i<nodes.size() ;i++){
+    //         for(int j=0 ; j<nodes.size() ; j++){
+    //             if(i==j){
+    //                 cost[i][j]=0;
+    //             }else{
+    //                 if
+    //                 cost[i][j]=arc[0].distance;
+    //             }
+    //         }
+    //     }
 
-    }
+    //     return cost;
+
+    // }
     /**
      * @return List<Segment> return the course
      */
