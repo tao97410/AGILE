1777746aef85e802082a232b22dd3111d98fa97c
Alban-Cognot <cognot.alban@gmail.com>
2023-12-05 11:05:33 +0100

    debut test graph

--- agile/src/test/java/h4131/model/MapTest.java
+++ agile/src/test/java/h4131/model/MapTest.java
@@ -3,6 +3,9 @@ package h4131.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
@@ -13,15 +16,15 @@ public class MapTest {
 
 
     @Test
-    void mockedMapAddIntersection() {
+    void TestAddIntersection() {
         mockedMap= new Map();
-        new Intersection(10,20,30);
-        mockedMap.addIntersection(new Intersection(10,20,30));
+        Intersection intersection = new Intersection(10,20,30);
+        mockedMap.addIntersection(intersection);
         assertNotNull(mockedMap.getIntersections().get((long)10));
     }
 
     @Test
-    void mockedMapAddSegment() { 
+    void TestAddSegment() { 
         mockedMap= new Map();
         Segment s= new Segment(new Intersection(10,20,30), new Intersection(20,30,40), 10, "Test");
         mockedMap.addSegment(s);
@@ -29,7 +32,7 @@ public class MapTest {
     }
 
     @Test
-    void mockedMapAddSegmentWithOriginExisting() { 
+    void TestAddSegmentWithOriginExisting() { 
         mockedMap= new Map();
         Segment s= new Segment(new Intersection(10,20,30), new Intersection(20,30,40), 10, "Test");
         mockedMap.addSegment(s);
@@ -39,61 +42,42 @@ public class MapTest {
 
     
     @Test
-    void mockedMapHasIntersection() {
+    void TestHasIntersection() {
         mockedMap= new Map();
         mockedMap.addIntersection(new Intersection(10,20,30));
         assertEquals(mockedMap.hasIntersection(10),true);
     }
     
 
+
     @Test
-    void mockedMapGetAdjacency() {
+    void TestGetGraphFromPointsWithoutPrivate() {
+        mockedMap = new Map();
+        DeliveryPoint mockWarehouse = new DeliveryPoint(new Intersection(1, 4,5), TimeWindow.WAREHOUSE);
+        DeliveryPoint mockFirsDeliveryPoint = new DeliveryPoint(new Intersection(2, 8,7), TimeWindow.WAREHOUSE);
+        DeliveryPoint mockSecondDeliveryPoint = new DeliveryPoint(new Intersection(3, 5,9), TimeWindow.WAREHOUSE);
+
+        Collection<DeliveryPoint> mockedDeliveryPoints = new LinkedList<>();
 
     }
 
     @Test
-    void mockedMapGetDestinationsById() {
+    void TestGetGraphFromPointsWithGetPath() {
 
     }
 
     @Test
-    void mockedMapGetGraphFromPoints() {
+    void TestGetGraphFromPointsWithFindSegment() {
 
     }
 
     @Test
-    void mockedMapGetIntersectionById() {
-
-    }
-
-    @Test
-    void mockedMapGetIntersections() {
-
-    }
-
-    @Test
-    void mockedMapGetWarehouse() {
+    void TestGetGraphFromPointsWithDijkstra() {
 
     }
 
 
-    @Test
-    void mockedMapSetAdjacency() {
 
-    }
-
-    @Test
-    void mockedMapSetIntersections() {
-
-    }
-
-    @Test
-    void mockedMapSetWarehouse() {
-
-    }
-
-    @Test
-    void mockedMapToString() {
-
-    }
+   
 }
+
